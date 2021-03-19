6c9f3da9cf6e9c8485ecb854daf27348abb82ece
Jhonny <jsoto@movilsoluciones.com.do>
2021-03-19 18:37:23 -0400

    commit main

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  // url: 'http://movilsoluciones_pos.test:8090/api',
-  // urlImagenes: 'http://movilsoluciones_pos.test:8090',
-  urlApi: 'https://vindecoder.p.rapidapi.com/v1.1/decode_vin?vin=',
-  url: 'http://mbs.movilsoluciones.com.do/api',
-  urlImagenes: 'http://mbs.movilsoluciones.com.do',
+   url: 'http://movilsoluciones_pos.test:8090/api',
+   urlImagenes: 'http://movilsoluciones_pos.test:8090',
+//  urlApi: 'https://vindecoder.p.rapidapi.com/v1.1/decode_vin?vin=',
+//  url: 'http://mbs.movilsoluciones.com.do/api',
+//  urlImagenes: 'http://mbs.movilsoluciones.com.do',
   min: 180000,
   max: 600000
 };
