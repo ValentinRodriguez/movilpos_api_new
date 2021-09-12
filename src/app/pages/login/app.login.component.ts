f9472fef5b15839ccab66dd52f9fb2857fc52d24
valentinrodriguez <valentinrodriguez1427@gmail.com>
2021-09-12 13:12:04 -0400

    last

--- src/app/pages/login/app.login.component.ts
+++ src/app/pages/login/app.login.component.ts
@@ -43,10 +43,11 @@ export class AppLoginComponent implements OnInit{
     this.usuarioServ.login(this.form).then((resp: any) => {
       this.usuarioServ.getMyOauthToken(this.form).then((resp2: any) => {
         const data = Object.assign(resp, resp2);
-        console.log(data);  
         this.usuarioServ.clearLocalStorage();      
         localStorage.setItem('e161d29e7b89977b32bf2d3f15a7200d',data.sessionId);       
-        this.usuarioServ.setLocalStorage(data); 
+        this.usuarioServ.setLocalStorage(data);
+        console.log(this.usuarioServ.getLocalStorage('e161d29e7b89977b32bf2d3f15a7200d'));
+        
       });
     });
   }
